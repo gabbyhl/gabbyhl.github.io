7407a757aa51a766b4e2ea25bbbcb25ad414d1cd
Gregory Croisdale <gregtc@umich.edu>
2024-07-23 21:59:26 -0400

    updated og

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Gabriela Holliman-Lopez';
+export const SITE_DESCRIPTION = `I'm a qualitative and mixed methods researcher who recently completed my MS in information science at the University of Michigan School of Information with a focus in Human-Computer Interaction and UX Research and Design. I'm passionate about understanding how and why people use technology, and turning that into actionable insights to design technology that is usable, helpful, and delightful.`;
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
